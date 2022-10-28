d859dd432f7af8cd6637de8db84c3ebd151185ad
abdurem <76563178+abdurem@users.noreply.github.com>
2022-10-28 21:19:58 +0300

    Time: 67 ms (91.62%), Space: 15.2 MB (25.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans={}
+        for i,n in enumerate(nums):
+            if target-n in ans:
+                return([ans[target-n],i])
+            else:
+                ans[n]=i
+                
+                
