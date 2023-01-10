daabb01c3e6c7694a3f0044d7a5673756565b32d
abdurem <76563178+abdurem@users.noreply.github.com>
2023-01-10 20:01:27 +0300

    Time: 34 ms (79.27%), Space: 13.9 MB (84.74%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -9,21 +9,17 @@ class Solution:
             while index < len(line):
                 char = line[index]
                 
-                # "//" -> Line comment.
                 if char == '/' and (index + 1) < len(line) and line[index + 1] == '/' and not skip_characters:
                     break 
                     
-                # "/*" -> Start of block comment.
                 elif char == '/' and (index + 1) < len(line) and line[index + 1] == '*' and not skip_characters:
                     skip_characters = True
                     index += 1
                     
-                # "*/" -> End of block comment.
                 elif char == '*' and (index + 1) < len(line) and line[index + 1] == '/' and skip_characters:
                     skip_characters = False
                     index += 1
                     
-                # Normal character. Append to buffer if not in block comment.
                 elif not skip_characters:
                     buffer += char
                     
