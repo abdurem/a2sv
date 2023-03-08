e1323160054ef73c5894d6c95e9746c3100a0b9e
abdurem <76563178+abdurem@users.noreply.github.com>
2023-03-08 17:52:38 +0300

    added binary-search-tree-to-greater-sum-tree.python

---
+++ binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.count = 0
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        if root == None:
+            return 0
+        
+        self.bstToGst(root.right)
+        self.count += root.val
+        root.val = self.count
+        
+        self.bstToGst(root.left)
+
+        return root
