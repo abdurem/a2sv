7d01926103f080d6fa6fc54779f79af09026de50
abdurem <76563178+abdurem@users.noreply.github.com>
2023-12-20 12:10:41 +0300

    Added solution for Unique Number of Occurrences

---
+++ camp/unique-number-of-occurrences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        c = Counter(arr)
+        s = set()
+
+        for i in c:
+            if c[i] in s:
+                return False
+            s.add(c[i])
+        
+        return True
